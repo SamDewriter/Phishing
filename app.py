8dace83c29cf16d6cc6c641631d278f34225b4cc
Mubaraq Sani <{ID}+{username}@users.noreply.github.com>
2023-07-02 16:13:45 +0100

    Pylint

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-import streamlit as st
-import pandas as pd
-import pickle
-import validators
-import copy
-import toml
-import base64
-from urllib.parse import urlparse, parse_qs
-from utils.extractor import FeaturesExtractor
+"""Smart Phishing Detection System using Machine Learning and Streamlit"""
 
+import base64
+import copy
+import pickle
+
+import pandas as pd
+import streamlit as st
+import toml
+import validators
+
+from utils.extractor import FeaturesExtractor
 
 # Load the model
 config = toml.load('config.toml')
@@ -19,8 +21,9 @@ with open(pickle_path, 'rb') as f:
     model = pickle.load(f)
 
 def is_single_column(file_path):
+    """Check if the uploaded CSV file has just one column."""
     try:
-        df = pd.read_csv(file_path)
+        df = pd.read_csv(file_path) # pylint: disable=invalid-name
         # if df.shape[1] == 1:
         #     return False
         return df.shape[1] == 1 and len(df.columns) > 0
@@ -28,25 +31,39 @@ def is_single_column(file_path):
         return False
     # except pd.errors.EmptyDataError:
     #     return False
-    
-def is_valid_url(url):
+
+def is_valid_url(url): # pylint: disable=R1710
+    """Check if the URL is valid."""
     try:
         if validators.url(url):
             return True
     except validators.ValidationFailure:
         return False
 
-def train_single(url):
+def predict_single_url(url):
+    """
+    Predict if the URL is a phishing URL or not.
+    
+    Args:
+        url (str): The URL to be checked.
+    Returns:
+        pred (int): The prediction result.    
+    """
     features = FeaturesExtractor(url).all_features()
-
-    df = pd.DataFrame([features], columns=columns)
-
+    df = pd.DataFrame([features], columns=columns) # pylint: disable=C0103
     pred = model.predict(df)
 
     return pred
 
-def train_multiple(df: pd.DataFrame):
+def predict_multiple_urls(df: pd.DataFrame): # pylint: disable=C0103
+    """
+    Predict if the URLs are phishing URLs or not.
 
+    Args:
+        df (pd.DataFrame): The DataFrame containing the URLs.
+    Returns:
+        pred_df (pd.DataFrame): The DataFrame containing the URLs and the predictions.
+    """
     pred_df = df.copy()
     features = []
     for url in df["URLs"]:
@@ -60,13 +77,15 @@ def train_multiple(df: pd.DataFrame):
     pred_df["Prediction"] = pred
 
     return pred_df
-    
+
 def main():
+    """Main function of the app."""
     st.title('Smart Phishing Detection System')
     st.image('phishing.jpg', width=700)
 
     st.sidebar.header('User Input Features')
-    st.sidebar.write('Please provide the following information to check if the website is a phishing website or not.')
+    st.sidebar.write("""Please provide the following information to
+                     check if the website is a phishing website or not.""")
 
     st.sidebar.header('Single URL Check')
     url = st.sidebar.text_input('Enter the URL here:')
@@ -84,7 +103,7 @@ def main():
                 st.write('The URL you entered is:')
                 st.write(url)
 
-                prediction = train_single(url)
+                prediction = predict_single_url(url)
 
                 if prediction[0] == 0:
                     st.write('This is a legitimate URL.')
@@ -100,14 +119,14 @@ def main():
 
             check_file = copy.copy(uploaded_file)
             if is_single_column(check_file):
-                df = pd.read_csv(uploaded_file)
+                df = pd.read_csv(uploaded_file) # pylint: disable=C0103
                 st.write("CSV file uploaded successfully")
 
                 st.write('---')
                 st.write('The URLs:')
                 st.write(df.head(10))
 
-                load_df = train_multiple(df)
+                load_df = predict_multiple_urls(df)
                 # Load the predictions
                 st.write('---')
                 st.write('Multiple Prediction Results')
@@ -117,29 +136,30 @@ def main():
                 # Do basic analysis on the predictions
                 st.subheader('Basic Analysis on the Prediction Results')
                 st.write('Total number of URLs:', load_df.shape[0])
-                st.write('Total number of Legitimate URLs:', load_df["Prediction"].value_counts()[0])
-                st.write('Total number of Phishing URLs:', load_df["Prediction"].value_counts()[1])
+                st.write('Total number of Legitimate URLs:',
+                         load_df["Prediction"].value_counts()[0])
+                st.write('Total number of Phishing URLs:',
+                         load_df["Prediction"].value_counts()[1])
 
 
                 # Download the predictions
                 st.write('---')
                 st.write('Download the prediction results here:')
                 csv = load_df.to_csv(index=False)
-                b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-                href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download CSV File</a>'
-                st.markdown(href, unsafe_allow_html=True)
-                # st.write(load_df.to_csv(index=False), encoding='utf-8', header=True)
 
+                # some strings <-> bytes conversions necessary here
+                b64 = base64.b64encode(csv.encode()).decode()
+                href = f"""<a href="data:file/csv;base64,{b64}"
+                                    download="prediction.csv">Download CSV File</a>"""
+                st.markdown(href, unsafe_allow_html=True)
 
             else:
-                # if is_single_column(uploaded_file) is False:
-                    # st.error("Error: The uploaded file is not compatible.")                    
                 st.sidebar.error("""
                     Error: The uploaded CSV File has more than one column.
                     Upload a CSV file with just one column.""")
 
                 # else:
                 #     st.error("Error: The uploaded CSV file has more than one column")
-            
+
 if __name__ == '__main__':
     main()
